3d1de0d2d28f0e6674624cb1ce98cd8bd4006d29
YaseenMunowwar <120727198+YaseenMunowwar@users.noreply.github.com>
2023-09-27 07:59:27 +0530

    Create app.py with basic imports

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def get_pdf_text(pdf_docs):
+     text = ""
+     for pdf in pdf_docs:
+          pdf_reader = PdfReader(pdf)
+          for page in pdf_reader.pages:
+               text += page.extract_text()
+
+     return text
+
 
 def main():
+     load_dotenv()
      st.set_page_config(page_title="Acumen PDF - Chat unlimited with Multiple PDFS",page_icon=":books:")
 
      st.header("Acumen PDF - Chat unlimited with Multiple PDFS:books:")
@@ -9,8 +21,17 @@ def main():
 
      with st.sidebar:
           st.subheader("Your Documents")
-          st.file_uploader("Upload your PDFs here and click on 'Process'")
-          st.button('Process')
+          pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'",accept_multiple_files=True)
+          if st.button('Process'):
+               with st.spinner("Processing"):
+                    #Extract the pdf texts
+                    
+                    raw_text = get_pdf_text(pdf_docs)
+
+                    #Extract the chunks of text
+
+
+                    #Create vector store
 
 
 
