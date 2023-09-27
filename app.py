bf928a9e56c782a09c1ba11358c4564576c91b74
YaseenMunowwar <120727198+YaseenMunowwar@users.noreply.github.com>
2023-09-27 11:03:29 +0530

    Creating FAISS Vector Store

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
 
 def get_pdf_text(pdf_docs):
+     'Function to read pdf and extract text from all uploaded PDFs'
      text = ""
      for pdf in pdf_docs:
           pdf_reader = PdfReader(pdf)
@@ -10,6 +15,26 @@ def get_pdf_text(pdf_docs):
                text += page.extract_text()
 
      return text
+
+
+def get_text_chunks(raw_text):
+     'Function to split raw extracted data into chunks using langchain'
+     text_splitter = CharacterTextSplitter(
+          separator="\n",
+          chunk_size=1000,
+          chunk_overlap=100,
+          length_function=len         
+     )
+
+     chunks = text_splitter.split_text(raw_text)
+     return chunks
+
+
+def get_vector_store(text_chunks):
+     'Create a vector store using FAISS'
+     embeddings = OpenAIEmbeddings()
+     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+     return vectorstore
 
 
 def main():
@@ -27,12 +52,14 @@ def main():
                     #Extract the pdf texts
                     
                     raw_text = get_pdf_text(pdf_docs)
-
+                    #st.write(raw_text)
+               
                     #Extract the chunks of text
-
+                    text_chunks = get_text_chunks(raw_text)
+                    st.write(text_chunks)
 
                     #Create vector store
-
+                    vector_store = get_vector_store(text_chunks)
 
 
 
